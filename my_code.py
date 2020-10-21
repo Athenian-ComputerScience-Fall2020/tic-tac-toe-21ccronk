b4da5d46cdafe4904d12811ca6011565110b016d
21ccronk <21ccronk@athenian.org>
2020-10-21 19:31:17 +0000

    Work in progress

--- my_code.py
+++ my_code.py
@@ -2,5 +2,13 @@
 #  
 # A note on style: Dictionaries can be defined before or after functions.
 
+boardd = {'TL',' ', 'TM',' ', 'TR',' ', 'ML',' ', 'MM',' ', 'MR',' ', 'BL',' ', 'BM',' ', 'BR',' '}
 
+def tictactoe(boardd):
+    print((boardd['TL']) + "|" + (boardd['TM']) + "|" + (boardd['TL']))
+    print("-+-+-")
+    print(" | | ")
+    print("-+-+-")
+    print(" | | ")
 
+tictactoe(boardd)
