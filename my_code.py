95e4b39c34bcb843b8a2fb26329ab5ee1fa2ab00
21ccronk <21ccronk@athenian.org>
2020-10-22 22:00:47 +0000

    Getting there

--- my_code.py
+++ my_code.py
@@ -2,13 +2,39 @@
 #  
 # A note on style: Dictionaries can be defined before or after functions.
 
-boardd = {'TL',' ', 'TM',' ', 'TR',' ', 'ML',' ', 'MM',' ', 'MR',' ', 'BL',' ', 'BM',' ', 'BR',' '}
+board = {'TL',' ', 'TM',' ', 'TR',' ', 'ML',' ', 'MM',' ', 'MR',' ', 'BL',' ', 'BM',' ', 'BR',' '}
 
-def tictactoe(boardd):
-    print((boardd['TL']) + "|" + (boardd['TM']) + "|" + (boardd['TL']))
+def tictactoe(board):
+    print((board['TL']) + "|" + (board['TM']) + "|" + (board['TL']))
     print("-+-+-")
-    print(" | | ")
+    print((board['ML']) + "|" + (board['MM']) + "|" + (board['ML']))
     print("-+-+-")
-    print(" | | ")
+    print((board['BL']) + "|" + (board['BM']) + "|" + (board['BR']))
 
-tictactoe(boardd)
+tictactoe(board)
+options = ['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR']
+
+a = str(input(print("Player One, where would you like to place your first X? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'X'})
+if a == 'TM':
+    board.update({'TM':'X'})
+if a == 'TR':
+    board.update({'TR':'X'})
+if a == 'ML':
+    board.update({'ML':'X'})
+if a == 'MM':
+    board.update({'MM':'X'})
+if a == 'MR':
+    board.update({'MR':'X'})
+if a == 'BL':
+    board.update({'BL':'X'})
+if a == 'BM':
+    board.update({'BM':'X'})
+if a == 'BR':
+    board.update({'BR':'X'})
+
+options.remove(a)
+
+tictactoe(board)
+
