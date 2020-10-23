dc805d85e5b105e4a7200789c6b8e9656f5681d4
21ccronk <21ccronk@athenian.org>
2020-10-23 01:51:35 +0000

    Almost there

--- my_code.py
+++ my_code.py
@@ -38,3 +38,195 @@ options.remove(a)
 
 tictactoe(board)
 
+a = str(input(print("Player Two, where would you like to place your first O? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'O'})
+if a == 'TM':
+    board.update({'TM':'O'})
+if a == 'TR':
+    board.update({'TR':'O'})
+if a == 'ML':
+    board.update({'ML':'O'})
+if a == 'MM':
+    board.update({'MM':'O'})
+if a == 'MR':
+    board.update({'MR':'O'})
+if a == 'BL':
+    board.update({'BL':'O'})
+if a == 'BM':
+    board.update({'BM':'O'})
+if a == 'BR':
+    board.update({'BR':'O'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player One, where would you like to place your next X? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'X'})
+if a == 'TM':
+    board.update({'TM':'X'})
+if a == 'TR':
+    board.update({'TR':'X'})
+if a == 'ML':
+    board.update({'ML':'X'})
+if a == 'MM':
+    board.update({'MM':'X'})
+if a == 'MR':
+    board.update({'MR':'X'})
+if a == 'BL':
+    board.update({'BL':'X'})
+if a == 'BM':
+    board.update({'BM':'X'})
+if a == 'BR':
+    board.update({'BR':'X'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player Two, where would you like to place your next O? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'O'})
+if a == 'TM':
+    board.update({'TM':'O'})
+if a == 'TR':
+    board.update({'TR':'O'})
+if a == 'ML':
+    board.update({'ML':'O'})
+if a == 'MM':
+    board.update({'MM':'O'})
+if a == 'MR':
+    board.update({'MR':'O'})
+if a == 'BL':
+    board.update({'BL':'O'})
+if a == 'BM':
+    board.update({'BM':'O'})
+if a == 'BR':
+    board.update({'BR':'O'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player One, where would you like to place your next X? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'X'})
+if a == 'TM':
+    board.update({'TM':'X'})
+if a == 'TR':
+    board.update({'TR':'X'})
+if a == 'ML':
+    board.update({'ML':'X'})
+if a == 'MM':
+    board.update({'MM':'X'})
+if a == 'MR':
+    board.update({'MR':'X'})
+if a == 'BL':
+    board.update({'BL':'X'})
+if a == 'BM':
+    board.update({'BM':'X'})
+if a == 'BR':
+    board.update({'BR':'X'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player Two, where would you like to place your next O? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'O'})
+if a == 'TM':
+    board.update({'TM':'O'})
+if a == 'TR':
+    board.update({'TR':'O'})
+if a == 'ML':
+    board.update({'ML':'O'})
+if a == 'MM':
+    board.update({'MM':'O'})
+if a == 'MR':
+    board.update({'MR':'O'})
+if a == 'BL':
+    board.update({'BL':'O'})
+if a == 'BM':
+    board.update({'BM':'O'})
+if a == 'BR':
+    board.update({'BR':'O'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player One, where would you like to place your next X? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'X'})
+if a == 'TM':
+    board.update({'TM':'X'})
+if a == 'TR':
+    board.update({'TR':'X'})
+if a == 'ML':
+    board.update({'ML':'X'})
+if a == 'MM':
+    board.update({'MM':'X'})
+if a == 'MR':
+    board.update({'MR':'X'})
+if a == 'BL':
+    board.update({'BL':'X'})
+if a == 'BM':
+    board.update({'BM':'X'})
+if a == 'BR':
+    board.update({'BR':'X'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player Two, where would you like to place your next O? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'O'})
+if a == 'TM':
+    board.update({'TM':'O'})
+if a == 'TR':
+    board.update({'TR':'O'})
+if a == 'ML':
+    board.update({'ML':'O'})
+if a == 'MM':
+    board.update({'MM':'O'})
+if a == 'MR':
+    board.update({'MR':'O'})
+if a == 'BL':
+    board.update({'BL':'O'})
+if a == 'BM':
+    board.update({'BM':'O'})
+if a == 'BR':
+    board.update({'BR':'O'})
+
+options.remove(a)
+
+tictactoe(board)
+
+a = str(input(print("Player One, where would you like to place your next X? Your options are " + options + " : ")))
+if a == 'TL':
+    board.update({'TL':'X'})
+if a == 'TM':
+    board.update({'TM':'X'})
+if a == 'TR':
+    board.update({'TR':'X'})
+if a == 'ML':
+    board.update({'ML':'X'})
+if a == 'MM':
+    board.update({'MM':'X'})
+if a == 'MR':
+    board.update({'MR':'X'})
+if a == 'BL':
+    board.update({'BL':'X'})
+if a == 'BM':
+    board.update({'BM':'X'})
+if a == 'BR':
+    board.update({'BR':'X'})
+
+options.remove(a)
+
+tictactoe(board)
+
