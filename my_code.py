be53d273f3462008cddd720d63e8b1aea435ae43
21ccronk <21ccronk@athenian.org>
2020-10-27 19:33:35 +0000

    Close to being done

--- my_code.py
+++ my_code.py
@@ -21,112 +21,166 @@ def winner(board, x):
         elif board['TL'] == 'O':
             print("Player Two Wins")
             x = x + 1
-        return x
     if board['ML'] == board['MM'] == board['MR']:
         if board['ML'] == 'X':
             print("Player One Wins")
-        if board['ML'] == 'O':
+            x = x + 1
+        elif board['ML'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['BL'] == board['BM'] == board['BR']:
         if board['BL'] == 'X':
             print("Player One Wins")
-        if board['BL'] == 'O':
+            x = x + 1
+        elif board['BL'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['TL'] == board['ML'] == board['BL']:
         if board['TL'] == 'X':
             print("Player One Wins")
-        if board['TL'] == 'O':
+            x = x + 1
+        elif board['TL'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['TM'] == board['MM'] == board['BM']:
         if board['TM'] == 'X':
             print("Player One Wins")
-        if board['TM'] == 'O':
+            x = x + 1
+        elif board['TM'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['TR'] == board['MR'] == board['BR']:
         if board['TR'] == 'X':
             print("Player One Wins")
-        if board['TR'] == 'O':
+            x = x + 1
+        elif board['TR'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['TL'] == board['MM'] == board['BR']:
         if board['TL'] == 'X':
             print("Player One Wins")
-        if board['TL'] == 'O':
+            x = x + 1
+        elif board['TL'] == 'O':
             print("Player Two Wins")
+            x = x + 1
     if board['TR'] == board['MM'] == board['BL']:
         if board['TR'] == 'X':
             print("Player One Wins")
-        if board['TR'] == 'O':
+            x = x + 1
+        elif board['TR'] == 'O':
             print("Player Two Wins")
+            x = x + 1
+    return x
 
 def playeroneturn(board, options):
     print("Player One, where would you like to place an X? Your options are " , options , " : ")
     a = input()
-    if a == 'TL':
-        board.update({'TL':'X'})
-    if a == 'TM':
-        board.update({'TM':'X'})
-    if a == 'TR':
-        board.update({'TR':'X'})
-    if a == 'ML':
-        board.update({'ML':'X'})
-    if a == 'MM':
-        board.update({'MM':'X'})
-    if a == 'MR':
-        board.update({'MR':'X'})
-    if a == 'BL':
-        board.update({'BL':'X'})
-    if a == 'BM':
-        board.update({'BM':'X'})
-    if a == 'BR':
-        board.update({'BR':'X'})
-
-    options.remove(a)
-
-    tictactoe(board)
+    if a in options:
+        if a == 'TL':
+            board.update({'TL':'X'})
+        if a == 'TM':
+            board.update({'TM':'X'})
+        if a == 'TR':
+            board.update({'TR':'X'})
+        if a == 'ML':
+            board.update({'ML':'X'})
+        if a == 'MM':
+            board.update({'MM':'X'})
+        if a == 'MR':
+            board.update({'MR':'X'})
+        if a == 'BL':
+            board.update({'BL':'X'})
+        if a == 'BM':
+            board.update({'BM':'X'})
+        if a == 'BR':
+            board.update({'BR':'X'})
+    
+        options.remove(a)
+        tictactoe(board)
+        return True
+    
+    elif a not in options: 
+        print("That space is not valid. Please try again.")
+        return False
 
 def playertwoturn(board, options):
     print("Player Two, where would you like to place an O? Your options are " , options , " : ")
     a = input()
-    if a == 'TL':
-        board.update({'TL':'O'})
-    if a == 'TM':
-        board.update({'TM':'O'})
-    if a == 'TR':
-        board.update({'TR':'O'})
-    if a == 'ML':
-        board.update({'ML':'O'})
-    if a == 'MM':
-        board.update({'MM':'O'})
-    if a == 'MR':
-        board.update({'MR':'O'})
-    if a == 'BL':
-        board.update({'BL':'O'})
-    if a == 'BM':
-        board.update({'BM':'O'})
-    if a == 'BR':
-        board.update({'BR':'O'})
+    if a in options:
+        if a == 'TL':
+            board.update({'TL':'O'})
+        if a == 'TM':
+            board.update({'TM':'O'})
+        if a == 'TR':
+            board.update({'TR':'O'})
+        if a == 'ML':
+            board.update({'ML':'O'})
+        if a == 'MM':
+            board.update({'MM':'O'})
+        if a == 'MR':
+            board.update({'MR':'O'})
+        if a == 'BL':
+            board.update({'BL':'O'})
+        if a == 'BM':
+            board.update({'BM':'O'})
+        if a == 'BR':
+            board.update({'BR':'O'})
 
-    options.remove(a)
+        options.remove(a)
+        tictactoe(board)
+        return True
+        
+    elif a not in options: 
+        print("That space is not valid. Please try again.")
+        return False
 
-    tictactoe(board)
 
 
 
 tictactoe(board)
 while x < 1:
-    playeroneturn(board, options)
-    playertwoturn(board, options)
-    playeroneturn(board, options)
-    playertwoturn(board, options)
-    playeroneturn(board, options)
-    winner(board,x) #why not ending while loop because x = 1?
-    playertwoturn(board, options)
-    winner(board,x)
-    playeroneturn(board, options)
-    winner(board,x)
-    playertwoturn(board, options)
-    winner(board,x)
-    playeroneturn(board, options)
+    y = playeroneturn(board, options)
+    while y == False:
+        playeroneturn(board,options)
+    y = playertwoturn(board, options)
+    while y == False:
+        playertwoturn(board,options)
+    y = playeroneturn(board, options)
+    while y == False:
+        playeroneturn(board,options)
+    y = playertwoturn(board, options)
+    while y == False:
+        playertwoturn(board,options)
+    y = playeroneturn(board, options)
+    while y == False:
+        playeroneturn(board,options)
+    x = winner(board,x) 
+    if x > 0:
+        break
+    y = playertwoturn(board, options)
+    while y == False:
+        playertwoturn(board,options)
+    x = winner(board,x)
+    if x > 0:
+        break
+    y = playeroneturn(board, options)
+    while y == False:
+        playeroneturn(board,options)
+    x = winner(board,x)
+    if x > 0:
+        break
+    y = playertwoturn(board, options)
+    while y == False:
+        playertwoturn(board,options)
+    x = winner(board,x)
+    if x > 0:
+        break
+    y = playeroneturn(board, options)
+    while y == False:
+        playeroneturn(board,options)
+    x = winner(board,x)
+    if x > 0:
+        break
     print("No one won this game")
     break
 
